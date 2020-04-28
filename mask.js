6654139bda73091548e3114ba5e3139f16c2e8cb
Juan Gonzalez <j3045771061@gmail.com>
2020-04-28 14:54:06 -0500

    upload teh page web

---
+++ mask.js
@@ -0,0 +1,6 @@
+const name = document.getElementById('text-name')
+var email = document.getElementById('text-email')
+var button = document.getElementById('button')
+button.addEventListener('click', drawing)
+
+function drawing() {}
